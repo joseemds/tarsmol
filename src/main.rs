2f086c89771d4e8b78f4d40d58b8d178167ecabd
José Eduardo Monteiro <mds.josee@gmail.com>
2022-12-04 02:17:21 -0300

    start reading tar files

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,43 @@
+use std::fs;
+
 // From https://www.gnu.org/software/tar/manual/html_node/Standard.html
-struct Header {
-    data: [u8; 512],
-};
-
-
-struct StarHeader{
-    name: [u8, 100],
-    mode: [u8, 8],
-    uid: [u8, 8],
-    gid: [u8, 8],
-    size: [u8,12],
-    msize: [u8,12],
-    chksum: [u8, 8],
+struct StarHeader {
+    name: [u8; 100],
+    mode: [u8; 8],
+    uid: [u8; 8],
+    gid: [u8; 8],
+    size: [u8; 12],
+    msize: [u8; 12],
+    chksum: [u8; 8],
     typeflag: u8,
-    linkname: [u8, 100],
-    magic: [u8, 6],
-    version: [u8, 2],
-    uname: [u8, 32],
-    gname: [u8, 32],
-    devmajor: [u8, 8],
-    devminor: [u8, 8],
-    prefix: [u8, 155],
-
-};
-
+    linkname: [u8; 100],
+    magic: [u8; 6],
+    version: [u8; 2],
+    uname: [u8; 32],
+    gname: [u8; 32],
+    devmajor: [u8; 8],
+    devminor: [u8; 8],
+    prefix: [u8; 155],
+}
 
 struct Archive {
     header: [u8; 512],
-
+    blocks: Vec<[u8; 512]>,
 }
-fn main() {
-    let x = Archive {
-        header: Default::default(),
 
-    };
+// impl Archive {
+//     fn from_file(file: &str) -> () {
+//         let content = fs::read(file).unwrap().chunks(512).collect::<Vec<_>>();
+//     }
+// }
+
+fn main() {
+    let tarball = fs::read("smol.tar").unwrap();
+    let entries = tarball.chunks(512).collect::<Vec<_>>();
+    println!("{:?}", entries);
+    let first_file = entries[0];
+    println!("File name: {}", std::str::from_utf8(&first_file[0..99]).unwrap().replace("\0", ""));
+    println!("File size: {:#o}", std::str::from_utf8(&first_file[124..135]).unwrap().replace("\0", "").parse::<i32>().unwrap());
+
     println!("Hello, world!");
 }
