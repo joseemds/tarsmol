68ddc884377511dca46648eda7a28de2e96e05ea
José Eduardo Monteiro <mds.josee@gmail.com>
2022-12-06 00:12:08 -0300

    add helper functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::str;
 
 // From https://www.gnu.org/software/tar/manual/html_node/Standard.html
 struct StarHeader {
@@ -30,14 +31,29 @@ struct Entry {
 }
 
 
+fn octal_to_decimal(num: &str) -> i32 {
+     i32::from_str_radix(num, 8).unwrap()
+}
+
+fn next_block(file_size: &str) -> i32 {
+    let file_size = octal_to_decimal(file_size);
+    if file_size % 512 != 0 {
+        (file_size / 512) + 2
+    } else {
+        (file_size / 512) + 1
+    }
+}
+
 fn main() {
     let tarball = fs::read("smol.tar").unwrap();
     let entries = tarball.chunks(512).collect::<Vec<_>>();
     println!("{:?}", entries);
     let first_file = entries[0];
-    println!("File name: {}", std::str::from_utf8(&first_file[0..99]).unwrap().replace("\0", ""));
-    println!("File mode: {}", std::str::from_utf8(&first_file[100..107]).unwrap().replace("\0", ""));
-    println!("File size: {}", std::str::from_utf8(&first_file[124..135]).unwrap().replace("\0", "").parse::<i32>().unwrap());
+    let first_file_size = std::str::from_utf8(&first_file[124..135]).unwrap().replace("\0", "");
+    println!("First File name: {}", std::str::from_utf8(&first_file[0..99]).unwrap().replace("\0", ""));
+    let next_file = next_block(first_file_size.as_str());
+    let second_file = entries[next_file as usize];
+    println!("Second File name: {}", std::str::from_utf8(&second_file[0..99]).unwrap().replace("\0", ""));
 
     println!("Hello, world!");
 }
